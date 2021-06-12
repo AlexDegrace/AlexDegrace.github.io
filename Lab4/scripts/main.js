4ac03e121f0910980900d08bc8ed0b9ffd5873a8
Alex Degrace <adegr091@uottawa.ca>
2021-06-12 11:28:05 -0400

    Finish lab 4

--- Lab4/scripts/main.js
+++ Lab4/scripts/main.js
@@ -1,2 +1,19 @@
 
 
+function submitForm(){
+	console.log("click")
+	document.getElementById("successAlert").classList.remove("collapse");
+	document.getElementById("successAlert").scrollIntoView();
+	document.getElementById("firstName").value = "";
+	document.getElementById("lastName").value = "";
+	document.getElementById("phone").value = "";
+	document.getElementById("email").value = "";
+	document.getElementById("date").value = "";
+}
+
+function goToBooking(bookType){
+	var elem = document.getElementById("selectService");
+	elem.value = bookType;
+	elem = document.getElementById("form");
+	elem.scrollIntoView();
+}
